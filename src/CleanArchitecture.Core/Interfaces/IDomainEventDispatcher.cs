093d001e05f4c8e4ec51b040e88ad70162435994
Ruchika Srivastava <ruchikasrivastava@hotmail.com>
2019-11-09 21:34:31 +0100

    Initial commit

---
+++ src/CleanArchitecture.Core/Interfaces/IDomainEventDispatcher.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CleanArchitecture.Core.SharedKernel;
+
+namespace CleanArchitecture.Core.Interfaces
+{
+    public interface IDomainEventDispatcher
+    {
+        Task Dispatch(BaseDomainEvent domainEvent);
+    }
+}
---
+++ tests/CleanArchitecture.Tests/NoOpDomainEventDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CleanArchitecture.Core.Interfaces;
+using CleanArchitecture.Core.SharedKernel;
+
+namespace CleanArchitecture.UnitTests
+{
+    public class NoOpDomainEventDispatcher : IDomainEventDispatcher
+    {
+        public Task Dispatch(BaseDomainEvent domainEvent)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
